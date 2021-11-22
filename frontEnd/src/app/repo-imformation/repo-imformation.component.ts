455a3b362c926eca6886f47a8210651b7dca4d57
110598010 <allen880612@gmail.com>
2021-11-22 11:00:00 +0800

    [Fixed] redirect analysis to choose-project

--- frontEnd/src/app/repo-imformation/repo-imformation.component.ts
+++ frontEnd/src/app/repo-imformation/repo-imformation.component.ts
@@ -9,23 +9,29 @@ import {Router, ActivatedRoute} from '@angular/router';
 export class RepoImformationComponent implements OnInit {
   owner: any;
   repoName = 'Repo1';
+  redirecturl: string;
 
 
   constructor(private router: Router,private activerouter: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    // this.activerouter.queryParams.subscribe( (Inputvalue:any) => {
-    //   this.owner  = Inputvalue['owner'].toString();
-    //   this.repoName  = Inputvalue['repoName'].toString();
-    // });
+    //console.log('repo');
+   // console.log(this.router.url.toString());
+    let current_url = this.router.url.toString();
+    if (current_url == '/code-base') {
+      this.redirecturl ='analysis';
+    }
+    else if (current_url == '/analysis'){
+      this.redirecturl ='choose-project';
+    }
+
     this.repoName = window.sessionStorage.getItem('repoName');
     this.owner = window.sessionStorage.getItem('owner');
   }
 
-
-
   NavitoAnalysis(){
-    this.router.navigateByUrl('analysis');
+   // console.log( 'r' + this.redirecturl);
+    this.router.navigateByUrl(this.redirecturl);
   }
 }
