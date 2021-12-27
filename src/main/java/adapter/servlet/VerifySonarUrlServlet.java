0b465c1112bb7f529749660139eff9e5bb46196f
allen880612 <allen880612@gmail.com>
2021-12-27 22:23:08 +0800

    [Add] Verify Sonar URL Servlet

---
+++ src/main/java/adapter/servlet/VerifySonarUrlServlet.java
@@ -0,0 +1,42 @@
+package adapter.servlet;
+
+import adapter.SonarQubeAccessorImpl;
+import org.json.JSONObject;
+import usecase.SonarQubeAccessor;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/verifySonarUrl", name = "verifySonarUrlServlet")
+public class VerifySonarUrlServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println(getClass().getName());
+
+        JSONObject requestBody = new JSONObject(request.getReader().readLine());
+        String host = requestBody.getString("host");
+        String projectKey = requestBody.getString("projectKey");
+        String token = requestBody.getString("token");
+
+        SonarQubeAccessor sonarAccessor = new SonarQubeAccessorImpl(host, projectKey, token);
+        boolean isUrlValid = sonarAccessor.isSonarProjectValid();
+
+        JSONObject returnJson = new JSONObject();
+        returnJson.put("isUrlValid", isUrlValid);
+
+        response.setContentType("text/json");
+        PrintWriter out = response.getWriter();
+        out.println(returnJson);
+        out.close();
+    }
+}
