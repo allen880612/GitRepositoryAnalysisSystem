58339874eb98ad524fdba30553a6feac63b83aa4
YuHong <87071007+yu6h@users.noreply.github.com>
2022-01-02 12:07:52 +0800

    新增Project的較抽象部分，還未完成，但感覺可以再討論看作法是否可行

--- src/main/java/adapter/project/CreateProjectUseCase.java
+++ src/main/java/adapter/project/CreateProjectUseCase.java
@@ -1,22 +1,45 @@
 package adapter.project;
 
 import domain.Project;
+import usecase.gitrepository.GitRepositoryRepository;
 import usecase.project.CreateProjectInput;
 import usecase.project.CreateProjectOutput;
 import usecase.project.ProjectRepository;
+import usecase.sonarproject.SonarProjectRepository;
 
 public class CreateProjectUseCase {
     private ProjectRepository projectRepository;
+    private GitRepositoryRepository gitRepositoryRepository;
+    private SonarProjectRepository sonarProjectRepository;
 
     public CreateProjectUseCase(ProjectRepository projectRepository) {
         this.projectRepository = projectRepository;
+
+    }
+    public CreateProjectUseCase(ProjectRepository projectRepository,GitRepositoryRepository gitRepositoryRepository
+            ,SonarProjectRepository sonarProjectRepository) {
+        this.projectRepository = projectRepository;
+        this.gitRepositoryRepository = gitRepositoryRepository;
+        this.sonarProjectRepository = sonarProjectRepository;
+        //GitDAO
+        //SonarDAO
     }
 
-    public void execute(CreateProjectInput input, CreateProjectOutput output){
-        Project project = new Project(input.getName(), input.getDescription());
+    public void execute(CreateProjectInput input, CreateProjectOutput output){//還沒完成，先下班晚點繼續
+        Project project = new Project(input.getName(), input.getDescription(),
+                this.gitRepositoryRepository.getGitRepositoryById().getId(),
+                this.sonarProjectRepository.getSonarProjectByProjectId().getId());
         projectRepository.createProject(project);
+
         output.setId(project.getId());
         output.setName(project.getName());
         output.setGitRepositories(project.getGitRepositories());
     }
+//    public void execute(CreateProjectInput input, CreateProjectOutput output){
+//        Project project = new Project(input.getName(), input.getDescription());
+//        projectRepository.createProject(project);
+//        output.setId(project.getId());
+//        output.setName(project.getName());
+//        output.setGitRepositories(project.getGitRepositories());
+//    }
 }
--- src/main/java/adapter/servlet/CreateProjectServlet.java
+++ src/main/java/adapter/servlet/CreateProjectServlet.java
@@ -2,17 +2,27 @@ package adapter.servlet;
 
 
 import adapter.account.AccountRepositoryImpl;
+import adapter.gitrepository.CreateGitRepositoryInputImpl;
+import adapter.gitrepository.CreateGitRepositoryOutputImpl;
+import adapter.gitrepository.GitRepositoryRepositoryImpl;
 import adapter.project.CreateProjectInputImpl;
 import adapter.project.CreateProjectOutputImpl;
 import adapter.project.CreateProjectUseCase;
 import adapter.project.ProjectRepositoryImpl;
+import adapter.sonarproject.SonarProjectRepositoryImpl;
 import domain.Account;
 import domain.Project;
+import domain.SonarProject;
 import org.json.JSONObject;
 import usecase.account.AccountRepository;
+import usecase.gitrepository.CreateGitRepositoryInput;
+import usecase.gitrepository.CreateGitRepositoryOutput;
+import usecase.gitrepository.CreateGitRepositoryUseCase;
+import usecase.gitrepository.GitRepositoryRepository;
 import usecase.project.CreateProjectInput;
 import usecase.project.CreateProjectOutput;
 import usecase.project.ProjectRepository;
+import usecase.sonarproject.SonarProjectRepository;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -48,14 +58,38 @@ public class CreateProjectServlet extends HttpServlet {
 
     private String createProjectAndReturnId(String userId, String projectName, String projectDescription){
         ProjectRepository projectRepository = new ProjectRepositoryImpl();
+        GitRepositoryRepository gitRepositoryRepository = new GitRepositoryRepositoryImpl();
+        SonarProjectRepository sonarProjectRepository = new SonarProjectRepositoryImpl();
+
         AccountRepository accountRepository = new AccountRepositoryImpl();
         CreateProjectInput input = new CreateProjectInputImpl();
         CreateProjectOutput output = new CreateProjectOutputImpl();
         input.setName(projectName);
         input.setDescription(projectDescription);
-        CreateProjectUseCase createProjectUseCase = new CreateProjectUseCase(projectRepository);
+
+//        CreateProjectUseCase createProjectUseCase = new CreateProjectUseCase(projectRepository);
+        CreateProjectUseCase createProjectUseCase = new CreateProjectUseCase(projectRepository,gitRepositoryRepository,sonarProjectRepository);
+
         createProjectUseCase.execute(input, output);
         String id = output.getId();
+
+        CreateGitRepositoryUseCase createGitRepositoryUseCase = new CreateGitRepositoryUseCase(gitRepositoryRepository);
+        CreateGitRepositoryInput createGitRepositoryInput = new CreateGitRepositoryInputImpl();
+        CreateGitRepositoryOutput createGitRepositoryOutput = new CreateGitRepositoryOutputImpl();
+
+        createGitRepositoryInput.setOwnerName("Peter Parker");
+        createGitRepositoryInput.setRepoName("Spider man");
+        createGitRepositoryUseCase.execute(createGitRepositoryInput,createGitRepositoryOutput);
+
+
+
+        sonarProjectRepository.createSonarProject(new SonarProject("url123","projectkey123","token123"),id);
+
+
+
+
+
+
 
         Account account = accountRepository.getAccountById(userId);
         account.addProject(id);
