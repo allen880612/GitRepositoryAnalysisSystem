c74fd95d247772b1f3e22eb819173e6cecea40a0
allen880612 <allen880612@gmail.com>
2022-01-07 20:19:06 +0800

    [Modify] Create Project and Test

--- src/main/java/adapter/servlet/CreateProjectServlet.java
+++ src/main/java/adapter/servlet/CreateProjectServlet.java
@@ -2,22 +2,19 @@ package adapter.servlet;
 
 
 import adapter.account.AccountRepositoryImpl;
-import adapter.account.CreateAccountInputImpl;
 import adapter.gitrepository.CreateGitRepositoryInputImpl;
 import adapter.gitrepository.CreateGitRepositoryOutputImpl;
 import adapter.gitrepository.GitRepositoryRepositoryImpl;
 import adapter.project.CreateProjectInputImpl;
 import adapter.project.CreateProjectOutputImpl;
 import adapter.sonarproject.CreateSonarProjectInputImpl;
-import domain.Project;
-import usecase.account.CreateAccountInput;
-import usecase.account.CreateAccountUseCase;
+import com.google.gson.Gson;
+import dto.CreateProjectServletDTO;
+import org.json.JSONObject;
 import usecase.project.CreateProjectUseCase;
 import adapter.project.ProjectRepositoryImpl;
 import adapter.sonarproject.SonarProjectRepositoryImpl;
 import domain.Account;
-import domain.SonarProject;
-import org.json.JSONObject;
 import usecase.account.AccountRepository;
 import usecase.gitrepository.CreateGitRepositoryInput;
 import usecase.gitrepository.CreateGitRepositoryOutput;
@@ -39,8 +36,10 @@ import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/createProject", name = "CreateProjectServlet")
 public class CreateProjectServlet extends HttpServlet {
-    class CreateProjectException extends Exception{
 
+    class CreateProjectException extends Exception{
+        CreateProjectException(String msg) { super(msg); }
+        CreateProjectException() { super(); }
     }
  
     @Override
@@ -50,74 +49,67 @@ public class CreateProjectServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        JSONObject jsonObject = new JSONObject();
-        JSONObject requestBody = new JSONObject(request.getReader().readLine());
+        String requestBody = request.getReader().readLine();
         System.out.println(requestBody);
-        String userId = String.valueOf(requestBody.get("userId"));
-        String projectName = String.valueOf(requestBody.get("projectName"));
-        String projectDescription = String.valueOf(requestBody.get("projectDescription"));
 
-        String githubUrl = String.valueOf(requestBody.get("githubUrl"));
-        String sonarHost = requestBody.getString("sonarHost");
-        String sonarToken = requestBody.getString("token");
-        String sonarProjectKey = requestBody.getString("sonarProjectKey");
-
+        JSONObject responseJson = new JSONObject();
+        Gson gson = new Gson();
         String projectId;
         boolean isSuccessful;
+        CreateProjectServletDTO requestDto = gson.fromJson(requestBody, CreateProjectServletDTO.class);
+
         try {
-            projectId = createProjectAndReturnId(userId, projectName, projectDescription, githubUrl, sonarHost, sonarToken, sonarProjectKey);
+            projectId = createProjectAndReturnId(requestDto.getProjectName(), requestDto.getProjectDescription());
+            addProjectOnUserID(requestDto.getUserId(), projectId);
+            createGitRepository(requestDto.getGithubUrl(), projectId);
+            useSonarProjectUseCase(requestDto.getSonarHost(), requestDto.getSonarToken(), requestDto.getSonarProjectKey(), projectId);
             isSuccessful = true;
-        }catch (CreateProjectException e){
+        } catch (CreateProjectException e) {
             projectId="";
             isSuccessful= false;
         }
-        jsonObject.put("projectId", projectId);
-        jsonObject.put("isSuccessful",isSuccessful);
+
+        responseJson.put("projectId", projectId);
+        responseJson.put("isSuccessful",isSuccessful);
         PrintWriter out = response.getWriter();
-        out.println(jsonObject);
+        out.println(responseJson);
         out.close();
     }
-    private String createProjectAndReturnId(String userId, String projectName, String projectDescription, String githubUrl, String sonarHost, String sonarToken, String sonarProjectKey) throws CreateProjectException {
-        String projectId = useProjectUseCaseAndReturnProjectID(projectName, projectDescription);
-        addProjectOnUserID(userId, projectId);
-        useGitRepositoryUseCase(githubUrl,projectId);
-        useSonarProjectUseCase(sonarHost,sonarToken,sonarProjectKey,projectId);
-        return projectId;
-    }
 
-    private void useGitRepositoryUseCase(String githubUrl,String projectId)throws CreateProjectException  {
+    private void createGitRepository(String githubUrl, String projectId) throws CreateProjectException  {
         GitRepositoryRepository gitRepositoryRepository = new GitRepositoryRepositoryImpl();
         CreateGitRepositoryInput input = new CreateGitRepositoryInputImpl();
         CreateGitRepositoryOutput output = new CreateGitRepositoryOutputImpl();
         CreateGitRepositoryUseCase createGitRepositoryUseCase = new CreateGitRepositoryUseCase(gitRepositoryRepository);
+
         input.setProjectID(projectId);
         input.setOwnerName(getRepoName(githubUrl, "github.com"));
         input.setRepoName(getRepoOwnerName(githubUrl, "github.com"));
         createGitRepositoryUseCase.execute(input, output);
-        if(!output.getIsSuccessful())throw new CreateProjectException();
 
+        if (!output.getIsSuccessful()) throw new CreateProjectException("create git repository fail");
     }
-    private String getRepoOwnerName(String validUrl, String keyWord){
+    private String getRepoOwnerName(String validUrl, String keyWord) throws CreateProjectException {
         String[] metadatas = validUrl.split("/");
         for (int i = 0; i < metadatas.length; i++) {
             if (metadatas[i].equals(keyWord)) {
                 return metadatas[i+2];
             }
         }
-        return null;
+        throw new CreateProjectException("parse repository owner name fail!");
     }
 
-    private String getRepoName(String validUrl, String keyWord){
+    private String getRepoName(String validUrl, String keyWord) throws CreateProjectException {
         String[] metadatas = validUrl.split("/");
         for (int i = 0; i < metadatas.length; i++) {
             if (metadatas[i].equals(keyWord)) {
                 return metadatas[i+1];
             }
         }
-        return null;
+        throw new CreateProjectException("parse repository name fail!");
     }
 
-    private String useProjectUseCaseAndReturnProjectID(String projectName, String projectDescription)throws CreateProjectException {
+    private String createProjectAndReturnId(String projectName, String projectDescription) throws CreateProjectException {
 
         ProjectRepository projectRepository = new ProjectRepositoryImpl();
 
@@ -130,10 +122,9 @@ public class CreateProjectServlet extends HttpServlet {
         CreateProjectUseCase createProjectUseCase = new CreateProjectUseCase(projectRepository);
         createProjectUseCase.execute(input, output);
 
-        if(!output.getIsSuccessful())throw new CreateProjectException();
-        String projectId = output.getId();
+        if(!output.getIsSuccessful()) throw new CreateProjectException("create project fail!");
 
-        return projectId;
+        return output.getId();
     }
 
 
@@ -151,19 +142,17 @@ public class CreateProjectServlet extends HttpServlet {
 
         CreateSonarProjectUseCase createSonarProjectUseCase = new CreateSonarProjectUseCase(sonarProjectRepository);
         createSonarProjectUseCase.execute(input,output);
-        if(!output.getIsSuccessful())throw new CreateProjectException();
+        if (!output.getIsSuccessful()) throw new CreateProjectException();
     }
 
     private void addProjectOnUserID(String userId,String projectId) throws CreateProjectException{
         AccountRepository accountRepository = new AccountRepositoryImpl();
         Account account = accountRepository.getAccountById(userId);
         account.addProject(projectId);
-
         try {
             accountRepository.updateAccountOwnProject(account);
         } catch (SQLException e) {
-            e.printStackTrace();
-            throw new CreateProjectException();
+            throw new CreateProjectException("add project to user fail.");
         }
     }
 }
---
+++ src/test/java/adapter/servlet/CreateProjectServletTest.java
@@ -0,0 +1,52 @@
+package adapter.servlet;
+
+import adapter.project.ProjectRepositoryImpl;
+import com.google.gson.Gson;
+import dto.CreateProjectServletDTO;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import usecase.project.ProjectRepository;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class CreateProjectServletTest {
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+    private CreateProjectServlet createProjectServlet;
+
+    @Before
+    public void setUp(){
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+        createProjectServlet = new CreateProjectServlet();
+
+        CreateProjectServletDTO createProjectServletDto = new CreateProjectServletDTO();
+        createProjectServletDto.setUserId("testUser");
+        createProjectServletDto.setProjectName("createProjectName");
+        createProjectServletDto.setProjectDescription("createProjectDescription");
+        createProjectServletDto.setGithubUrl("https://github.com/allen880612/GitRepositoryAnalysisSystem");
+        createProjectServletDto.setSonarHost("sonarHost");
+        createProjectServletDto.setSonarProjectKey("sonarProjectKey");
+        createProjectServletDto.setSonarToken("sonarToken");
+
+        request.addHeader("Content-Type","text/json");
+        request.setContent(new Gson().toJson(createProjectServletDto).getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Test
+    public void GetPersonalCommitsStatsTest() throws IOException, ServletException {
+        createProjectServlet.doPost(request, response);
+        JSONObject jsonObject = new JSONObject(response.getContentAsString());
+        Assert.assertTrue(jsonObject.getBoolean("isSuccessful"));
+        String projectId = jsonObject.getString("projectId");
+        Assert.assertNotNull(projectId);
+        ProjectRepository projectRepository = new ProjectRepositoryImpl();
+        projectRepository.deleteProject(projectId);
+    }
+}
